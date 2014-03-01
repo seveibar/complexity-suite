8caa05356d906ed84ed555419fdc4f47fb7dd078
seveibar <seveibar@gmail.com>
2014-03-01 18:56:14 -0500

    Dynamic simulation loading and urls working

--- playground.js
+++ playground.js
@@ -1,10 +1,26 @@
 
 var playground = function(){
 
+	// Restore playground to original state
+
+	function reset(){
+		playground.context.fillStyle = "#ccc";
+		playground.context.fillRect(0,0,600,500);
+		playground.ui.reset();
+		playground.value_changed = doNothing;
+		playground.vars = {};
+	}
+
 	// Sets title of playground (changes html element)
 
 	function set_title(title){
 		$("#title-bar").text(title);
+	}
+
+	function loadSimulation(simulationName){
+		console.log("Called with ",simulationName);
+		playground.reset();
+		$.getScript("simulations/" + simulationName + ".js");
 	}
 
 	function doNothing(){};
@@ -17,20 +33,25 @@ var playground = function(){
 		width : 600,
 		height : 500,
 
+		reset : reset,
 		set_title: set_title,
-		value_changed : doNothing
+		value_changed : doNothing,
+		load : loadSimulation
 	};
 }();
 
 $(document).ready(function(){
-	
+
 	var canvas = document.getElementById("canvas");
 	var context = canvas.getContext('2d');
 
-	context.fillStyle = "#ccc";
-	context.fillRect(0,0,600,500);
-
 	playground.canvas = canvas;
 	playground.context = context;
+
+	// Autoload simulation from URL bar
+
+	var simulation_name = window.location.href.split("#")[1] || "xyslider";
+	
+	playground.load(simulation_name);
 
 });
--- simulations/sinwave.js
+++ simulations/sinwave.js
@@ -1,37 +1,31 @@
 
-$(document).ready(function(){
-	
-	console.log(playground);
+// Add some test ui elements
 
-	// Add some test ui elements
+playground.set_title("Sin wave");
 
-	playground.set_title("Sin Wave");
+playground.ui.add_slider("posx",
+	0,100);
 
-	playground.ui.add_slider("posx",
-		0,100);
+playground.ui.add_slider("posy",
+	0,100);
 
-	playground.ui.add_slider("posy",
-		0,100);
+function render(){
+	var c = playground.context;
+	var w = playground.width;
+	var h = playground.height;
 
-	function render(){
-		var c = playground.context;
-		var w = playground.width;
-		var h = playground.height;
+	var posx = playground.vars.posx;
+	var posy = playground.vars.posy;
 
-		var posx = playground.vars.posx;
-		var posy = playground.vars.posy;
+	c.fillStyle = "#fff";
+	c.fillRect(0,0,w,h);
 
-		c.fillStyle = "#fff";
-		c.fillRect(0,0,w,h);
+	c.fillStyle = "#000";
+	c.fillRect(posx/100 * w,posy / 100 * h,10,10);
 
-		c.fillStyle = "#000";
-		c.fillRect(posx/100 * w,posy / 100 * h,10,10);
+}
 
-	}
+playground.value_changed = render;
 
-	playground.value_changed = render;
-
-	// Initial Render
-	render();
-
-});
+// Initial Render
+render();
