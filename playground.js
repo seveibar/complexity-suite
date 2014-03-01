5d8779280952fa6a62ca5310212d6b2cd61cd4da
seveibar <seveibar@gmail.com>
2014-03-01 02:57:09 -0500

    Created playground files and slider ui element, began work on api

---
+++ playground.js
@@ -0,0 +1,28 @@
+
+var playground = function(){
+	return {
+		ui : null,
+		canvas : null,
+		context : null,
+		width : 600,
+		height : 500
+	};
+}();
+
+window.onload = function(){
+	var canvas = document.getElementById("canvas");
+	var context = canvas.getContext('2d');
+
+	context.fillStyle = "#ccc";
+	context.fillRect(0,0,600,500);
+
+	playground.canvas = canvas;
+	playground.context = context;
+
+	playground.ui.add_slider("Time Step",
+		0,100);
+
+	playground.ui.add_slider("Speed",
+		20,1000);
+
+};
---
+++ playground_ui.js
@@ -0,0 +1,37 @@
+playground.ui = function(){
+
+	function add_slider(name, min, max, callback, initial){
+		
+		var slider_container = $("<div class='slider-container'></div>"); 
+		var slider_text = $("<div class='slider-text'>"+name+"</div>");
+		var value_text = $(" <span></span>");
+
+		slider_text.append(value_text);
+		slider_container.append(slider_text);
+
+
+		var slider = $("<div class='slider'></div>");
+
+		slider_container.append(slider);
+		$("#control-panel").append(slider_container);
+
+		slider.slider({
+			min : min,
+			max : max,
+			change : function (event, ui){
+				value_text.text(" ["+slider.slider("value")+"]");
+				callback && callback(slider.slider("value"));
+			}
+		});
+
+		slider.slider("value",(initial || (min + max)/2));
+
+		value_text.text(" ["+slider.slider("value")+"]");
+
+	}
+
+	return {
+		add_slider : add_slider,
+		add_selection : null
+	};
+}();
