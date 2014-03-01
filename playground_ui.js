548ae12f6dff2d9eca6bab15c612f74f8e5d4f94
seveibar <seveibar@gmail.com>
2014-03-01 18:39:35 -0500

    Reset method to remove all ui elements and some function modularization

--- playground_ui.js
+++ playground_ui.js
@@ -1,5 +1,15 @@
 playground.ui = function(){
 
+	var containers = [];
+
+	// Remove all ui elements
+	function reset(){
+		_.each(containers, function(container){
+			container.remove();
+		});
+		containers = [];
+	}
+
 	// Add a element to the playground display
 
 	function add_slider(name, min, max, callback, initial){
@@ -17,25 +27,43 @@ playground.ui = function(){
 		slider_container.append(slider);
 		$("#control-panel").append(slider_container);
 
+		function updateLabelText(){
+			value_text.text(" ["+slider.slider("value")+"]");
+		}
+
+		function sliderValueChange(event, ui){
+
+			updateLabelText();
+
+			// Update playground variable
+			playground.vars[name] = slider.slider("value");
+
+			// If there was a callback call it
+			callback && callback(slider.slider("value"));
+
+			// Let the playground know it will have to redraw
+			playground.value_changed();
+
+		}
+
 		slider.slider({
 			min : min,
 			max : max,
-			slide : function (event, ui){
-				value_text.text(" ["+slider.slider("value")+"]");
-				playground.vars[name] = slider.slider("value");
-				callback && callback(slider.slider("value"));
-				playground.value_changed();
-			}
+			slide : sliderValueChange
 		});
+
+		// Set initial values
 
 		slider.slider("value",(initial || (min + max)/2));
 		playground.vars[name] = slider.slider("value");
+		updateLabelText();
 
-		value_text.text(" ["+slider.slider("value")+"]");
+		containers.push(slider_container);
 
 	}
 
 	return {
+		reset : reset,
 		add_slider : add_slider,
 		add_selection : null
 	};
